d5e437f4acd87ac2d252e69e9cc167ff08429c4b
AmmarKhan09 <ammarkhanofficial9@gmail.com>
2024-09-13 17:32:19 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('thoughts-deleter-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/images/icon-192.png',
+        '/images/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
